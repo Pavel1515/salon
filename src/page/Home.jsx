5150178708dd1e0121cefb37421bdd6f390de2f2
Pavlo Yerebakan <ppvr3407@gmail.com>
2024-11-16 14:47:06 +0200

    +

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,14 +1,21 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "../style/index.scss";
 import { useNavigate } from "react-router-dom";
 import videoFile from "../assets/video/video.mp4";
+import english from "../assets/image/flags/english.jpg";
+import thai from "../assets/image/flags/taiwan.jpg";
+import russian from "../assets/image/flags/ru.jpg";
+import chinese from "../assets/image/flags/china.jpg";  
 
 const Home = () => {
-  const videoRef = useRef(null); 
+  const videoRef = useRef(null);
   const navigate = useNavigate();
+  const [selectedLanguage, setSelectedLanguage] = useState(null); // Состояние для выбранного языка
 
   // Функция для воспроизведения видео и перехода
-  const handleLinkClick = (path) => {
+  const handleLinkClick = (path, language) => {
+    setSelectedLanguage(language); // Устанавливаем выбранный язык
+
     if (videoRef.current) {
       videoRef.current.play(); // Проигрываем видео
 
@@ -29,24 +36,37 @@ const Home = () => {
             height="240"
             src={videoFile}
             type="video/mp4"
+            playsInline // Добавлено чтобы отключить полноэкранный режим на iPhone
           />
         </div>
         <div className="container_buttons">
-          <span className="font_english">
-            <button onClick={() => handleLinkClick("/english")} className="link-button">
-              English
+          <span className={`font_english ${selectedLanguage && selectedLanguage !== 'english' ? 'hidden' : ''}`}>
+            <button
+              onClick={() => handleLinkClick("/english", "english")}
+              className={`link-button`}
+            >
+             <img src={english} alt="en" />
             </button>
           </span>
-          <button onClick={() => handleLinkClick("/thai")} className="link-button">
-            ภาษาไทย
+          <button
+            onClick={() => handleLinkClick("/thai", "thai")}
+            className={`link-button ${selectedLanguage && selectedLanguage !== 'thai' ? 'hidden' : ''}`}
+          >
+            <img src={thai} alt="th" />
           </button>
-          <span className="font_english">
-            <button onClick={() => handleLinkClick("/russian")} className="link-button">
-              Русский
+          <span className={`font_english ${selectedLanguage && selectedLanguage !== 'russian' ? 'hidden' : ''}`}>
+            <button
+              onClick={() => handleLinkClick("/russian", "russian")}
+              className={`link-button`}
+            >
+              <img src={russian} alt="ru" />
             </button>
           </span>
-          <button onClick={() => handleLinkClick("/chinese")} className="link-button">
-            中文
+          <button
+            onClick={() => handleLinkClick("/chinese", "chinese")}
+            className={`link-button ${selectedLanguage && selectedLanguage !== 'chinese' ? 'hidden' : ''}`}
+          >
+            <img src={chinese} alt="china" />
           </button>
         </div>
       </div>
