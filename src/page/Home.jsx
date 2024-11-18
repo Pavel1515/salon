c47c0684305ed30ed638ff7ec83a02b94d81bcd6
Pavlo Yerebakan <ppvr3407@gmail.com>
2024-11-18 19:00:44 +0200

    +

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
             return prevIndex;
           }
         });
-      }, 40); 
+      }, 60); 
     }
     return () => clearInterval(interval);
   }, [isPlaying, svgFiles.length]);
